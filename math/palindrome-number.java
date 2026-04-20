32e8375acac978901d666c67ffe1bae06c5ea0e5
Keertan Nayak <184207663+keertanayak@users.noreply.github.com>
2026-04-20 20:32:42 +0530

    Update palindrome-number.java

--- math/palindrome-number.java
+++ math/palindrome-number.java
@@ -1,1 +1,18 @@
+// Return True if a given integer is a palindrome number
 
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0) {
+            return false;
+        }
+        int digit;
+        int rev=0;
+        int temp=x;
+        while(temp!=0) {
+            digit=temp%10;
+            rev=rev*10+digit;
+            temp/=10;
+        }
+        return x==rev;
+    }
+}
