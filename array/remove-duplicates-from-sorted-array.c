ecc8d130c500311afcfa0a1a4945c240f31e7a6d
Keertan Nayak <184207663+keertanayak@users.noreply.github.com>
2026-04-20 20:42:49 +0530

    Update remove-duplicates-from-sorted-array.c

--- array/remove-duplicates-from-sorted-array.c
+++ array/remove-duplicates-from-sorted-array.c
@@ -1,1 +1,14 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize==0)
+        return 0;
+    int pos=1; // next posn to put new number
+    for(int i=1; i<numsSize; i++){
+        if(nums[i] != nums[i-1]){ // to check if current is different from previous element
+            nums[pos] = nums[i];
+            pos++;
+        }
+    }
+    return pos;
+}
