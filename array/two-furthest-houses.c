192da9b928d52aae956470bb5761f726fb9b1563
Keertan Nayak <184207663+keertanayak@users.noreply.github.com>
2026-04-20 20:15:07 +0530

    two-furthest-houses soln
    Implement function to calculate maximum distance between two houses of different colors.

--- array/two-furthest-houses.c
+++ array/two-furthest-houses.c
@@ -1,1 +1,11 @@
-
+// Return Max distance between two houses of different colors
+int maxDistance(int* colors, int colorsSize) {
+    int max = 0;
+    for(int i = 0; i<colorsSize; i++){
+        if(colors[i] != colors[0] && i>max)  // compare with with the first house and change distance
+            max=i;
+        if(colors[i] != colors[colorsSize-1] && (colorsSize-1-i)>max)  // compare with last house (colorsSize - 1)
+            max=colorsSize-1-i;
+    }
+    return max;
+}
